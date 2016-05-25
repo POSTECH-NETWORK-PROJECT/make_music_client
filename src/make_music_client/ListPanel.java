c311ab76ed2690d5deefd54ed891b7ad71b1f452
user <user@dell>
2016-05-25 13:14:40 +0900

    Show room name

--- src/make_music_client/ListPanel.java
+++ src/make_music_client/ListPanel.java
@@ -3,6 +3,7 @@ package make_music_client;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
@@ -12,24 +13,28 @@ import javax.swing.JPanel;
 public class ListPanel {
 	JPanel panel;
 	DefaultListModel<String> rooms;
+	ArrayList<String> roomNames;
 	JList<String> roomList;
 	
 	public void refresh() throws IOException {
 		String msg;
 		rooms.clear();
+		roomNames.clear();
 		
 		MainFrame.server.sendShowRoomListSignalToServer();
 		while ((msg=MainFrame.server.getMessageFromServer()) != null) {
 			if (msg.indexOf("@END") == 0)
 				break;
 			
-			rooms.addElement(msg);
+			roomNames.add(msg);
+			rooms.addElement(msg.substring(0, msg.indexOf(":")));
 		}
 	}
 	
 	public ListPanel() throws IOException {
 		panel = new JPanel();
 		rooms = new DefaultListModel<String>();
+		roomNames = new ArrayList<String>();
 		
 		panel.setLayout(null);
 		
@@ -41,8 +46,8 @@ public class ListPanel {
 		btnJoin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try {
-					System.out.println(rooms.get(roomList.getSelectedIndex()).toString());
-					String roomName = rooms.get(roomList.getSelectedIndex());
+					System.out.println(roomNames.get(roomList.getSelectedIndex()).toString());
+					String roomName = roomNames.get(roomList.getSelectedIndex());
 					
 					RoomPanel room = new RoomPanel(roomName.substring(roomName.indexOf(":") + 1));
 					
